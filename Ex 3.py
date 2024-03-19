3e5b3772fa7c98069f59a9162afe190906027518
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-03-19 15:22:02 +0300

    Guess Game - Commit 1 Enriched code further

--- Ex 3.py
+++ Ex 3.py
@@ -9,12 +9,18 @@ print(((2**6)*'..'))
 print("Lets play a guessing game. You guess a letter and we check if it makes up one of the letters in our randomly generated word!\n")
 print(f'The randomy generated word is, {chosen_word}\n')
 
+#creating blanks for every randomly selected word
+display=[]
+
+for x in chosen_word:
+    display.append('-')
+
 guess="" #created a holding variable for every guessed letter by users
 
 #loop statement to count number of guesses that is equal to chosen random word
 for x in chosen_word:
-
-    user_query=input(print("Guess a 'letter' or character making a word: "))
+    print(display)
+    user_query=input(print(f"Guess a 'letter' or character making a word. You have {len(display)} guesses"))
     user_query.lower()
     guess+=user_query
     print(f'You entered, {user_query}. Checking')
@@ -22,9 +28,11 @@ for x in chosen_word:
         #str()
         print("Letter exist")
         print(f'\nYour made up word is {guess}',end=' ')
+        print(f'Our randomly selected word is, {chosen_word}')
     else:
         print("Letter don't exist")
         print(f'\nYour made up word is {guess}',end=' ')
+        print(f'Our randomly selected word is, {chosen_word}')
 
 
 
