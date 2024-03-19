b132db2a4afedf9994f9bcae5cb7b279dda3b592
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-03-19 15:03:39 +0300

    Guess Game - Commit 1 Enriched code further

--- Ex 3.py
+++ Ex 3.py
@@ -6,24 +6,25 @@ word_list=["avocado","baboon","camel"]
 #randomly selected word from the initial list
 chosen_word=random.choice(word_list)
 print(((2**6)*'..'))
+print("Lets play a guessing game. You guess a letter and we check if it makes up one of the letters in our randomly generated word!\n")
 print(f'The randomy generated word is, {chosen_word}\n')
 
 guess="" #created a holding variable for every guessed letter by users
 
 #loop statement to count number of guesses that is equal to chosen random word
-for x in len(chosen_word)
+for x in chosen_word:
 
-print("Lets play a guessing game. You guess a letter and we check if it makes up one of the letters in our randomly generated word!\n")
-user_query=input(print("Guess a 'letter' or character making a word: "))
-
-guess=user_query.lower()
-
-
-print(f'You entered, {guess}. Checking')
-if guess in chosen_word:
-    print("Letter exist")
-else:
-    print("Letter don't exist")
+    user_query=input(print("Guess a 'letter' or character making a word: "))
+    user_query.lower()
+    guess+=user_query
+    print(f'You entered, {user_query}. Checking')
+    if x==user_query:
+        #str()
+        print("Letter exist")
+        print(f'\nYour made up word is {guess}',end=' ')
+    else:
+        print("Letter don't exist")
+        print(f'\nYour made up word is {guess}',end=' ')
 
 
 
