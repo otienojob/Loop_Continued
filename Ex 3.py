f1003bdf539551236b888a137fe56f148063d909
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-03-20 07:44:10 +0300

    Guess Game - Commit 1 Enriched code further

--- Ex 3.py
+++ Ex 3.py
@@ -1,30 +1,37 @@
 import random
 
 #word list
+chosen_word=[]
 word_list=["avocado","baboon","camel"]
-
-#randomly selected word from the initial list
+chosen_word.append(random.choice(word_list))
+holding_letter=""
 
 print(((2**6)*'..'))
-print("Lets play a guessing game. You guess a letter and we check if it makes up one of the letters in our randomly generated word!\n")
-chosen_word=random.choice(word_list)
+print(f'Hint: Our randomly generated word is, {chosen_word}\n')
+
+blanks=[]
+
 word_length=len(chosen_word)
 
-print(f'The randomy generated word is, {chosen_word}\n')
-
-#creating blanks for every randomly selected word
-display=[]
-
 for x in range(word_length):
-    display.append('-')
-
+    blanks.append('-')
 guess="" #created a holding variable for every guessed letter by users
 
+print("Lets play a guessing game. You guess a letter and we check if it makes up one of the letters in our randomly generated word!\n")
+print(blanks)
+
+
+user_query=input(print(f"Guess a 'letter' or character making our mystery word. You have {word_length} guesses"))
+user_query.lower()
+guess += user_query
+#creating blanks for every randomly selected word
+
+
 #loop statement to count number of guesses that is equal to chosen random word
-for y in range(word_length):
+for y in chosen_word:
     holding_letter=chosen_word[y]
-    print(display)
-    user_query=input(print(f"\n Guess a 'letter' or character making a word. You have {len(display)} guesses"))
+    print(f'First guess is, {user_query}, while {y} mystery word is {chosen_word(y)}')
+    user_query=input(print(f"\n Guess a 'letter' or character making a word. You have {y} guesses remaining"))
     user_query.lower()
     guess+=user_query
     print(f'You entered, {user_query}. Checking')
