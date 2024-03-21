35261dd743c12d19a6c96fbe6bb050fd0f7b0d63
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-03-21 11:38:59 +0300

    Added new proj

--- Ex 5.py
+++ Ex 5.py
@@ -15,4 +15,21 @@ for x in range(0,word_length):
 print(f"Hint: Our Mystery Word Is: {random_selector}")
 print((2**5)*'-')
 print(blanks)
-user_input=input(print("Guess the letters making up the mystery word"))
+#user_input=input(print("Guess the letters making up the mystery word"))
+#user_input.lower()
+
+for y in range(0,word_length):
+    letter=random_selector[y]
+    #print(letter,end="")
+    user_input = input(print("Guess the letters making up the mystery word"))
+    user_input.lower()
+    if letter==user_input:
+        print("Correct Guess")
+        blanks[y]=user_input
+        print(blanks)
+    else:
+        print("Incorrect Guess!")
+        blanks[y] = user_input
+        print(blanks)
+
+
